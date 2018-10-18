4a4d8fbb24dc21e52d72ab09c10c2c60cca0699b
aisuhua <aisuhua@users.noreply.github.com>
2018-10-18 22:26:43 +0800

    Update send.php

--- demo09/demo01/send.php
+++ demo09/demo01/send.php
@@ -4,13 +4,13 @@ require (__DIR__ . '/../init.php');;
 // 队列名称
 $queue_name = 'my_queue';
 
-// 字符串类型的消息
+// 字符串消息
 $payload = implode(' ', array_slice($argv, 1));
 // 将消息发送到队列
 $added = cls_rabbitmq::publish($queue_name, $payload, 'rabbitmq');
 var_dump($added);
 
-// 数组类型的消息
+// 数组消息
 $payload = array_slice($argv, 1);
 // 将消息发送到队列
 $added = cls_rabbitmq::publish($queue_name, $payload, 'rabbitmq');
